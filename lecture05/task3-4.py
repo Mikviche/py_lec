d777f10775a0d13e869ed5a0e9bca8045c7576e0
Mikviche <mm.asail.mm@gmail.com>
2023-10-26 00:15:19 +0700

    modified dices a bit, added task 4 to task3-4 file

--- lecture05/task3-4.py
+++ lecture05/task3-4.py
@@ -1,4 +1,5 @@
 import dices as d
+import random as rd
 while True:
     input_dices = input("Enter dices in format <N1dM1 N2dM2 ...>  (note that N<100): ")
     if all(map(d.check, input_dices.split())):
@@ -11,7 +12,13 @@ dices = []
 for dice in input_dices.split():
     dices.append(d.convert(dice))
 
+print("Probabilities for dice values sums:")
 probstable = d.probs(dices)
 for i in probstable:
     print(f"{i:3}: {f'{round(probstable[i],3):.3f}':6} %")
+
+print("Your roll:")
+for dice in dices:
+    for _ in range(1,dice[0]+1):
+        print(f"d{dice[1]:<3}-> {rd.randint(1,dice[1])}")
 
